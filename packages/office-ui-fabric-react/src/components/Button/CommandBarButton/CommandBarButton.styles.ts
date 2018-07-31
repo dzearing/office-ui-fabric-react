2519a8a73acc0e765be9cd8ab47a03717876d549
David Zearing <dzearing@microsoft.com>
2018-07-31 15:37:23 -0700

    Adding examples of theming problems.

--- packages/office-ui-fabric-react/src/components/Button/CommandBarButton/CommandBarButton.styles.ts
+++ packages/office-ui-fabric-react/src/components/Button/CommandBarButton/CommandBarButton.styles.ts
@@ -1,148 +1,142 @@
-import { IButtonStyles } from '../Button.types';
+import { IButtonStyles, IButtonStyleProps } from '../Button.types';
 import { ITheme, concatStyleSets, getFocusStyle, HighContrastSelector } from '../../../Styling';
-import { memoizeFunction } from '../../../Utilities';
-import { getStyles as getBaseButtonStyles } from '../BaseButton.styles';
-import { getStyles as getSplitButtonStyles } from '../SplitButton/SplitButton.styles';
 
-export const getStyles = memoizeFunction(
-  (theme: ITheme, customStyles?: IButtonStyles, focusInset?: string, focusColor?: string): IButtonStyles => {
-    const baseButtonStyles: IButtonStyles = getBaseButtonStyles(theme);
-    const baseSplitButtonStyles: IButtonStyles = getSplitButtonStyles(theme);
-    const commandButtonHighContrastFocus = {
-      left: 4,
-      top: 4,
-      bottom: 4,
-      right: 4,
-      border: 'none'
-    };
+export function getStyles(props: IButtonStyleProps): IButtonStyles {
+  const { theme } = props;
+  const { palette } = theme;
 
-    const commandButtonStyles: IButtonStyles = {
-      root: [
-        getFocusStyle(theme, -1, 'relative', commandButtonHighContrastFocus),
-        {
-          minWidth: '40px',
-          backgroundColor: theme.palette.neutralLighter,
-          color: theme.palette.neutralPrimary,
-          padding: '0 4px',
-          selectors: {
-            [HighContrastSelector]: {
-              border: 'none'
-            }
-          }
-        }
-      ],
+  const commandButtonHighContrastFocus = {
+    left: 4,
+    top: 4,
+    bottom: 4,
+    right: 4,
+    border: 'none'
+  };
 
-      rootHovered: {
-        backgroundColor: theme.palette.neutralLight,
-        color: theme.palette.neutralDark,
-        selectors: {
-          [HighContrastSelector]: {
-            color: 'Highlight'
-          }
-        }
-      },
-
-      rootPressed: {
-        backgroundColor: theme.palette.neutralQuaternaryAlt,
-        color: theme.palette.black
-      },
-
-      rootChecked: {
-        backgroundColor: theme.palette.neutralQuaternaryAlt,
-        color: theme.palette.black
-      },
-
-      rootExpanded: {
-        backgroundColor: theme.palette.neutralQuaternaryAlt,
-        color: theme.palette.black
-      },
-
-      rootCheckedHovered: {
-        backgroundColor: theme.palette.neutralQuaternary,
-        color: theme.palette.black
-      },
-
-      // Split button styles
-      splitButtonContainer: {
+  return {
+    root: [
+      getFocusStyle(theme, -1, 'relative', commandButtonHighContrastFocus),
+      {
+        minWidth: '40px',
+        backgroundColor: palette.neutralLighter,
+        color: palette.neutralPrimary,
+        padding: '0 4px',
         selectors: {
           [HighContrastSelector]: {
             border: 'none'
           }
         }
-      },
+      }
+    ],
 
-      splitButtonDivider: {
-        backgroundColor: theme.palette.neutralTertiaryAlt,
-        marginTop: 4,
-        marginBottom: 4
-      },
+    rootHovered: {
+      backgroundColor: palette.neutralLight,
+      color: palette.neutralDark,
+      selectors: {
+        [HighContrastSelector]: {
+          color: 'Highlight'
+        }
+      }
+    },
 
-      splitButtonMenuButton: {
-        backgroundColor: theme.palette.neutralLighter,
-        color: theme.palette.neutralPrimary,
-        selectors: {
-          ':hover': {
-            backgroundColor: theme.palette.neutralLight,
-            color: theme.palette.neutralDark,
-            selectors: {
-              [HighContrastSelector]: {
-                color: 'Highlight'
-              }
+    rootPressed: {
+      backgroundColor: palette.neutralQuaternaryAlt,
+      color: palette.black
+    },
+
+    rootChecked: {
+      backgroundColor: palette.neutralQuaternaryAlt,
+      color: palette.black
+    },
+
+    rootExpanded: {
+      backgroundColor: palette.neutralQuaternaryAlt,
+      color: palette.black
+    },
+
+    rootCheckedHovered: {
+      backgroundColor: palette.neutralQuaternary,
+      color: palette.black
+    },
+
+    // Split button styles
+    splitButtonContainer: {
+      selectors: {
+        [HighContrastSelector]: {
+          border: 'none'
+        }
+      }
+    },
+
+    splitButtonDivider: {
+      backgroundColor: palette.neutralTertiaryAlt,
+      marginTop: 4,
+      marginBottom: 4
+    },
+
+    splitButtonMenuButton: {
+      backgroundColor: palette.neutralLighter,
+      color: palette.neutralPrimary,
+      selectors: {
+        ':hover': {
+          backgroundColor: palette.neutralLight,
+          color: palette.neutralDark,
+          selectors: {
+            [HighContrastSelector]: {
+              color: 'Highlight'
             }
           }
         }
-      },
-
-      splitButtonMenuButtonDisabled: {
-        backgroundColor: theme.palette.neutralLighter,
-        selectors: {
-          ':hover': {
-            backgroundColor: theme.palette.neutralLighter
-          }
-        }
-      },
-
-      splitButtonMenuButtonChecked: {
-        backgroundColor: theme.palette.neutralQuaternaryAlt,
-        color: theme.palette.black,
-        selectors: {
-          ':hover': {
-            backgroundColor: theme.palette.neutralQuaternaryAlt
-          }
-        }
-      },
-
-      splitButtonMenuButtonExpanded: {
-        backgroundColor: theme.palette.neutralQuaternaryAlt,
-        color: theme.palette.black,
-        selectors: {
-          ':hover': {
-            backgroundColor: theme.palette.neutralQuaternaryAlt
-          }
-        }
-      },
-
-      splitButtonMenuIcon: {
-        color: theme.palette.neutralPrimary
-      },
-
-      splitButtonMenuIconDisabled: {
-        color: theme.palette.neutralTertiary
-      },
-
-      label: {
-        fontWeight: 'normal' // theme.fontWeights.semibold,
-      },
-
-      icon: {
-        color: theme.palette.themeDarkAlt
-      },
-
-      menuIcon: {
-        color: theme.palette.neutralSecondary
       }
-    };
+    },
 
-    return concatStyleSets(baseButtonStyles, commandButtonStyles, baseSplitButtonStyles, customStyles)!;
-  }
-);
+    splitButtonMenuButtonDisabled: {
+      backgroundColor: palette.neutralLighter,
+      selectors: {
+        ':hover': {
+          backgroundColor: palette.neutralLighter
+        }
+      }
+    },
+
+    splitButtonMenuButtonChecked: {
+      backgroundColor: palette.neutralQuaternaryAlt,
+      color: palette.black,
+      selectors: {
+        ':hover': {
+          backgroundColor: palette.neutralQuaternaryAlt
+        }
+      }
+    },
+
+    splitButtonMenuButtonExpanded: {
+      backgroundColor: palette.neutralQuaternaryAlt,
+      color: palette.black,
+      selectors: {
+        ':hover': {
+          backgroundColor: palette.neutralQuaternaryAlt
+        }
+      }
+    },
+
+    splitButtonMenuIcon: {
+      color: palette.neutralPrimary
+    },
+
+    splitButtonMenuIconDisabled: {
+      color: palette.neutralTertiary
+    },
+
+    label: {
+      fontWeight: 'normal' // theme.fontWeights.semibold,
+    },
+
+    icon: {
+      color: palette.themeDarkAlt
+    },
+
+    menuIcon: {
+      color: palette.neutralSecondary
+    }
+  } as IButtonStyles;
+}
--- packages/office-ui-fabric-react/src/components/CommandBar/examples/CommandBar.Basic.Example.tsx
+++ packages/office-ui-fabric-react/src/components/CommandBar/examples/CommandBar.Basic.Example.tsx
@@ -1,28 +1,118 @@
 import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import {
+  CommandBar,
+  ContextualMenuItemType,
+  Customizer,
+  DefaultButton,
+  ICommandBarStyles
+} from 'office-ui-fabric-react';
 
-import { CommandBar, ICommandBarProps } from 'office-ui-fabric-react/lib/CommandBar';
-
-export class CommandBarBasicExample extends React.Component<ICommandBarProps, {}> {
-  constructor(props: ICommandBarProps) {
-    super(props);
-    this.state = {
-      areNamesVisible: true,
-      areIconsVisible: true
-    };
-  }
-
-  public render(): JSX.Element {
-    const { items, overflowItems, farItems } = this.props;
-
+export class CommandBarBasicExample extends React.Component<{}> {
+  public render() {
     return (
-      <div>
-        <CommandBar
-          items={items}
-          overflowItems={overflowItems}
-          farItems={farItems}
-          ariaLabel={'Use left and right arrow keys to navigate between commands'}
-        />
-      </div>
+      <Customizer
+        scopedSettings={{
+          ContextualMenuItem: {
+            styles: {
+              root: {
+                backgroundColor: 'red'
+              },
+              rootHovered: {
+                backgroundColor: '#8b8cc7',
+                color: '#fff'
+              },
+              divider: {
+                marginTop: 4,
+                marginBottom: 4
+              }
+            }
+          },
+          CommandBar: {
+            styles: {
+              root: {
+                backgroundColor: 'red',
+                background: 'red',
+                border: '2px solid blue'
+              }
+            } as ICommandBarStyles
+          },
+          CommandBarButton: {
+            root: {
+              background: 'inherit !important',
+              border: '5px solid black'
+            }
+          }
+        }}
+      >
+        <div>
+          <div>hello world</div>
+          <button>button</button>
+          <CommandBar items={[{ name: 'asdf', key: 'asdf' }, { name: 'asaqwe', key: 'lolsd' }]} />
+          <DefaultButton
+            id="ContextualMenuButton1"
+            text="Click for ContextualMenu"
+            menuProps={{
+              shouldFocusOnMount: true,
+              items: [
+                {
+                  key: 'newItem',
+                  text: 'New',
+                  onClick: () => console.log('New clicked')
+                },
+                {
+                  key: 'divider_1',
+                  itemType: ContextualMenuItemType.Divider
+                },
+                {
+                  key: 'rename',
+                  text: 'Rename',
+                  onClick: () => console.log('Rename clicked')
+                },
+                {
+                  key: 'edit',
+                  text: 'Edit',
+                  onClick: () => console.log('Edit clicked')
+                },
+                {
+                  key: 'properties',
+                  text: 'Properties',
+                  onClick: () => console.log('Properties clicked')
+                },
+                {
+                  key: 'linkNoTarget',
+                  text: 'Link same window',
+                  href: 'http://bing.com'
+                },
+                {
+                  key: 'linkWithTarget',
+                  text: 'Link new window',
+                  href: 'http://bing.com',
+                  target: '_blank'
+                },
+                {
+                  key: 'linkWithOnClick',
+                  name: 'Link click',
+                  href: 'http://bing.com',
+                  onClick: (ev: React.MouseEvent<HTMLAnchorElement | HTMLButtonElement>) => {
+                    alert('Link clicked');
+                    ev.preventDefault();
+                  },
+                  target: '_blank'
+                },
+                {
+                  key: 'disabled',
+                  text: 'Disabled item',
+                  disabled: true,
+                  onClick: () => console.error('Disabled item should not be clickable.')
+                }
+              ]
+            }}
+          />
+        </div>
+      </Customizer>
     );
   }
 }
+
+ReactDOM.render(<CommandBarBasicExample />, document.getElementById('content'));
